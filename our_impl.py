1620ca40b31f02cdc9dd8bfe8b5226e30962d48a
divy-kala <divy.kala@students.iiit.ac.in>
2019-10-09 02:49:05 +0530

    transmission added

--- our_impl.py
+++ our_impl.py
@@ -44,14 +44,39 @@ for i in range(h):
 #ATM_LIGHT = max((sum(I[i, j, :]) for _, i, j in HEAP))
 max_val = -1
 #optimization: only need to check n/2 leaves, because leaves of a heap contain the max value
-ATM_LIGHT = np.zeros([1, 3])
+#ATM_LIGHT = np.zeros([1, 3])
 for _, i, j in HEAP:
 	sm = sum(I[i, j, :])
 	if sm > max_val:
 		max_val = sm
 		ATM_LIGHT = I[i, j, :]
 		
+				
+#transmission
+omega = 0.95
+temp_I = np.empty(I.shape,I.dtype)
+for i in range(3):
+	temp_I[:, :, i] = I[:, :, i]/ATM_LIGHT[i]
+
+dark_trans = np.zeros([h, w])	
+for i in range(h):
+	for j in range(w):
+		left_limit = max(j - step, 0)
+		right_limit = min(j + step, w-1)
+		top_limit = max(i - step, 0)
+		bottom_limit = min(i+step, h-1)
+		B = temp_I[top_limit:bottom_limit, left_limit:right_limit, 0]
+		G = temp_I[top_limit:bottom_limit, left_limit:right_limit, 1]
+		R = temp_I[top_limit:bottom_limit, left_limit:right_limit, 2]
+		
+		min_color = np.minimum(np.minimum(B, G), R)
+		min_val = np.min(min_color)
+		dark_trans[i, j] = min_val
+
+transmission = 1 - omega*dark_trans
+	
 		
 cv2.imshow("dark", dark)
+cv2.imshow("trans", transmission)
 cv2.waitKey()
  
